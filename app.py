a31e3282bf622c04d198541f7f4886cf2abfd47a
wnsrlf0721 <wnsrlf0721@ajou.ac.kr>
2025-08-16 11:05:36 +0900

    edit deploy error

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.base import BaseCallbackHandler
+from pathlib import Path
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -32,7 +33,8 @@ class ChatCallbackHandler(BaseCallbackHandler):
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
-    with open(file_path, "wb") as f:
+    Path("./.cache/files").mkdir(parents=True, exist_ok=True)
+    with open(file_path, "wb+") as f:
         f.write(file_content)
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
     splitter =CharacterTextSplitter.from_tiktoken_encoder(
@@ -66,6 +68,8 @@ def paint_history():
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
+def load_memory(input):
+    return memory.load_memory_variables({})["history"]
 
 prompt = ChatPromptTemplate.from_messages([
     ("system","""
@@ -73,12 +77,40 @@ prompt = ChatPromptTemplate.from_messages([
 
     Context: {context}
     """),
-    MessagesPlaceholder(variable_name = "chat_history"),
     ("human","{question}"),
 ])
 
-def load_memory(input):
-    return memory.load_memory_variables({})["history"]
+def main():
+    if not api_key:
+        st.error("API Key를 입력하지 않았습니다.")
+        return
+    
+    llm = ChatOpenAI(
+        temperature=0.1, 
+        streaming= True,
+        api_key = api_key,
+        callbacks=[
+            ChatCallbackHandler(),
+        ]
+    )
+
+    if file:
+        retriever = embed_file(file)
+        send_message("I'm ready! Ask away!", "ai",save=False)
+        paint_history()
+        message = st.chat_input("Ask anything about your file...")
+        if message:
+            send_message(message, "human")
+            chain = {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            } | prompt | llm
+            with st.chat_message("ai"):
+                result = chain.invoke(message)
+    else:
+        st.session_state["messages"]= []
+        return
+
 
 st.title("DocumentGPT")
 
@@ -95,40 +127,8 @@ with st.sidebar:
     api_key = st.text_input("Put your API key", type="password")
     file = st.file_uploader("Upload a .txt, .pdf or .docx file", type=["pdf","txt","docx"],)
 
-
-if file:
-    if not api_key:
-        st.error("API Key를 입력하지 않았습니다.")
-    else:
-        llm = ChatOpenAI(
-            temperature=0.1, 
-            streaming= True,
-            api_key = api_key,
-            callbacks=[
-                ChatCallbackHandler(),
-            ]
-        )
-
-        #전체 prompt를 메모리에 저장하는 방식 활용
-        memory = ConversationBufferMemory(
-            llm = llm,
-            return_messages=True
-        )
-
-        retriever = embed_file(file)
-        send_message("I'm ready! Ask away!", "ai",save=False)
-        paint_history()
-        message = st.chat_input("Ask anything about your file...")
-        if message:
-            send_message(message, "human")
-            chain = {
-                "context": retriever | RunnableLambda(format_docs),
-                "question": RunnablePassthrough(),
-                "chat_history": load_memory
-            } | prompt | llm
-            with st.chat_message("ai"):
-                result = chain.invoke(message)
-                memory.save_context({"question": message},{"output": result.content})
-    
-else:
-    st.session_state["messages"]=[]
+try:
+    main()
+except Exception as e:
+    st.error("Check your OpenAI API Key or File")
+    st.write(e)
